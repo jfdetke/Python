70b3b6f4b0f502b55ea314f10377a8a7f56f791e
john detke <jdetke@gmail.com>
2020-10-12 14:30:25 -0700

    checking in found fizzbuzz.py

--- HackerRank/sample1/fizzbuzz.py
+++ HackerRank/sample1/fizzbuzz.py
@@ -21,7 +21,7 @@ def fizzBuzz(n):
             print("FizzBuzz")
         elif (i % 3 == 0):
             print("Fizz")
-        elif (i % 5 == 0):
+        mlif (i % 5 == 0):
             print("Buzz")
         else:
             print(i)
